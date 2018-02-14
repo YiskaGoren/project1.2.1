a9de19ed162cb7872429d8bda7c2993efef07572
yiska <yiskagoen@gamil.com>
2018-02-14 20:15:22 +0200

    new db.json

--- src/db/dbInviter.ts
+++ src/db/dbInviter.ts
@@ -1,7 +1,8 @@
-export class DbInviter{
+export class DbInviter{//להוריד
     id: number;
     inviterId:string;
     name: string;
     mailAddress:string;
     Phone:string;
+    idInviter:number;
 }
